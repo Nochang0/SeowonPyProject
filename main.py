4297bd681a06d45d67cf2f095a9e09d8852c74f7
Nochang0 <hy040504@kakao.com>
2023-12-01 05:28:28 +0000

    프로젝트 완료

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib import ytdl
 # flask 라이브러리에서 호출할 요소들 (클래스, 함수)
 from flask import Flask, request, send_file, send_from_directory, jsonify
 
-# 
+# CORS를 쉽게 처리하기 위해 사용하는 코드
 from flask_cors import CORS
 
 
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # 모든 경로에 대해 CORS(Cross-Origin Resource Sharing)를 활성화합니다.
 # 웹 앱에서 발생하는 동일 출처 정책(Same-Origin Policy)을 우회하여 다른 출처의 리소스에 접근할 수 있도록 함
 # 동일 출처 정책: 웹 앱에서 다른 출처(프로토콜, 도메인, 포트)의 리소스에 접근하는 것을 제한
-# AJAX를 이용하면 웹 페이지의 일부분만을 서버로부터 비동기적으로 요청하고, 해당 부분만을 업데이트할 수 있습니다. 
+# AJAX를 이용하면 웹 페이지의 일부분만을 서버로부터 비동기적으로 요청하고, 해당 부분만을 업데이트할 수 있습니다.
 CORS(app)
 
 # 메인 다운로드 홈페이지
@@ -28,7 +28,7 @@ def main():
     return send_from_directory('./static/html/main', 'index.html')
 
 
-# 다운로드 API
+# 유튜브 다운로드 사이트
 @app.route('/youtube', methods=['GET'])
 def download_media():
     # 유튜브 영상 URL과 파일 형식을 받아옴 (파라미터)
@@ -38,7 +38,7 @@ def download_media():
 
     # 유튜브 주소(url) 또는 파일 형식(form)을 입력하지 않았을 떄 오류
     if not url or not form:
-        return jsonify({ "Error" : "유튜브 링크와 파일 형식을 모두 입력하세요." })
+        return "유튜브 링크와 파일 형식을 모두 입력하세요."
 
     # 유튜브 다운로드
     media = YT.download(url, form)
@@ -51,9 +51,9 @@ def download_media():
 
     # 다운로드 오류가 발생했을 때
     if media == False:
-        return jsonify({ "Error" : "다운로드에 실패했습니다. 다시 시도해주세요." })
+        return "다운로드에 실패했습니다. 다시 시도해주세요."
     else:
-        return jsonify({ "Error" : "다운로드에 성공했습니다." })
+        return "다운로드에 성공했습니다."
 
 # main.py가 직접 실행될 때 실행되어야 하는 코드
 if __name__ == '__main__':
